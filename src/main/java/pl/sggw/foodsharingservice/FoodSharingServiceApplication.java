f98e508eeb0f9a1a357ee446636a34fbdcfaa68b
Marcin Bratek <marcin.bratek@akmf.pl>
2021-08-19 17:10:03 +0200

    initial-commit

---
+++ src/main/java/pl/sggw/foodsharingservice/FoodSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.sggw.foodsharingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSharingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSharingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sggw/foodsharingservice/FoodSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sggw.foodsharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSharingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
