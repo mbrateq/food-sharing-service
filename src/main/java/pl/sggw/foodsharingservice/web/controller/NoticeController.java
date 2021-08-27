f99682591a07aeed8f1e15f65805e97ced3c2832
Marcin Bratek <marcin.bratek@akmf.pl>
2021-08-27 16:50:40 +0200

    fixed-test

--- src/main/java/pl/sggw/foodsharingservice/web/controller/NoticeController.java
+++ src/main/java/pl/sggw/foodsharingservice/web/controller/NoticeController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class NoticeController implements NoticeOperations {
 
-    @GetMapping("/")
+//    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "Hello";
     }
